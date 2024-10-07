c80ad660e335e5ba3e75252238488eea5b45c9fb
Ty Clifford <861810+snick512@users.noreply.github.com>
2024-10-06 22:02:40 -0400

    -

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 // Connect to the MySQL database
-$host = "";
-$dbname = "";
-$username = ""; // Change this to your database username
-$password = ""; // Change this to your database password
+$host = " ";
+$dbname = " ";
+$username = " "; // Change this to your database username
+$password = " "; // Change this to your database password
 
 ?>
